98def9f74a081d634334f227b19643c99cfbf272
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-05-31 20:11:00 -0300

    removed underscore

--- Route.go
+++ Route.go
@@ -16,24 +16,24 @@ type Route struct {
 func (route *Route) IsMatch(url string) bool {
 	switch route._t {
 	case byte(2):
-		return route._isMatchRegExp(url)
+		return route.isMatchRegExp(url)
 	case byte(1):
-		return route._isMatchRemainder(url)
+		return route.isMatchRemainder(url)
 	case byte(0):
-		return route._isMatchExact(url)
+		return route.isMatchExact(url)
 	}
 	return false
 }
 
-func (route *Route) _isMatchRegExp(url string) bool {
+func (route *Route) isMatchRegExp(url string) bool {
 	match, _ := regexp.MatchString(route.Path, url)
 	return match
 }
 
-func (route *Route) _isMatchExact(url string) bool {
+func (route *Route) isMatchExact(url string) bool {
 	return url == route.Path
 }
 
-func (route *Route) _isMatchRemainder(url string) bool {
-	return strings.Index(url, route.Path) == 0
+func (route *Route) isMatchRemainder(url string) bool {
+	return strings.HasPrefix(url, route.Path)
 }
